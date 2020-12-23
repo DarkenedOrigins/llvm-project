ae1efcad99b90a7734ace0cd5ed5df0705f3f593
dark <darkenblade986@gmail.com>
2020-12-22 21:33:17 -0600

    finally got it to open and close on click

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Table from './Table.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 class App extends Component {
 
@@ -11,6 +13,7 @@ class App extends Component {
 		}
 	}
 
+	//OVERHERE CHANGE THE IP TO THE PROPER ADDRESS
 	componentDidMount() {
 		fetch('http://172.17.200.193:8080/file_list')
 			.then(res => res.json())
@@ -20,7 +23,7 @@ class App extends Component {
 			});} );
 		//console.log(this.state.fileNames, "over here");
 	}
-
+	//OVERHERE CHANGE THE IP TO THE PROPER ADDRESS
 	getFdata(filename){
 		fetch('http://172.17.200.193:8080/process/'+filename.split(".")[0])
 			.then(res => res.json())
@@ -31,10 +34,10 @@ class App extends Component {
 				let cConv = "";
 				let iCount = "";
 				for (var i=0; i<data.length; i++){
-					funcs += data[i].name + ",";
-					rType += data[i].returnType + ",";
-					cConv += data[i].callConv + ",";
-					iCount += data[i].instCount + ",";
+					funcs += data[i].name + ", ";
+					rType += data[i].returnType + ", ";
+					cConv += data[i].callConv + ", ";
+					iCount += data[i].instCount + ", ";
 				}
 				var fdata = { 'func':funcs, 'rType':rType, 'cConv': cConv, 'iCount':iCount  };
 				this.setState({'fileData': [...this.state.fileData, fdata]});
--- react-app/src/Table.js
+++ react-app/src/Table.js
@@ -1,32 +1,74 @@
 import React from 'react';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Table = ({filenames, fdata}) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>File Name</th>
-          <th>Functions</th>
-          <th>Return Types</th>
-          <th>Call Conventions</th>
-          <th>Instruction Count Per</th>
-        </tr>
-      </thead>
-      <tbody>
-	  { (filenames.length > 0) ? fdata.map( (data, index) => {
-           return (
-            <tr key={ index }>
-              <td>{ filenames[index] }</td>
-              <td>{ data.func }</td>
-              <td>{ data.rType}</td>
-              <td>{ data.cConv }</td>
-              <td>{ data.iCount }</td>
-            </tr>
-          )
-         }) : <tr><td colSpan="5">Loading...</td></tr> }
-      </tbody>
-    </table>
-  );
+const Table = ({ filenames, fdata }) => {
+	return (
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th>File Name</th>
+					{/* <th>Functions</th>
+					<th>Return Types</th>
+					<th>Call Conventions</th>
+					<th>Instruction Count Per</th> */}
+				</tr>
+			</thead>
+			<tbody>
+				{(filenames.length > 0) ? fdata.map((data, index, array) => {
+					//console.log(array, "over here");
+					let index1 = index + array.length;
+					let index2 = index + 2*array.length;
+					let index3 = index + 3*array.length;
+					let index4 = index + 4*array.length;
+					let rId = "accordion"+index.toString();
+					let tar = "#"+rId;
+					return (
+						[
+							<tr key={index} data-toggle="collapse" data-target={tar} class="clickable">
+								<td>{filenames[index]}</td>
+								{/* <td>{data.func}</td>
+								<td>{data.rType}</td>
+								<td>{data.cConv}</td>
+								<td>{data.iCount}</td> */}
+							</tr>,
+
+							<tr key={index1}>
+								<td colSpan="5">
+									<div id={rId} class="collapse">
+										Functions: {data.func}
+									</div>
+								</td>
+							</tr>,
+
+							<tr key={index2}>
+								<td colSpan="5">
+									<div id={rId} class="collapse">
+										Return Types: {data.rType}
+									</div>
+								</td>
+							</tr>,
+
+							<tr key={index3}>
+								<td colSpan="5">
+									<div id={rId} class="collapse">
+										Call Conventions: {data.cConv}
+									</div>
+								</td>
+							</tr>,
+
+							<tr key={index4}>
+								<td colSpan="5">
+									<div id={rId} class="collapse">
+										Instruction Count: {data.iCount}
+									</div>
+								</td>
+							</tr>
+						]
+					)
+				}) : <tr><td colSpan="5">Loading...</td></tr>}
+			</tbody>
+		</table>
+	);
 }
 
 export default Table
